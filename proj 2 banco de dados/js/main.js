95e273e7d0607ee81948b46ee7d7f6e7b336c838
Leonardo Pontes Gaspar <730550228@adm.unifor.br@DTEC65046812.unifor.br>
2023-05-11 15:47:57 -0300

    fixing more regex bugs

--- proj 2 banco de dados/js/main.js
+++ proj 2 banco de dados/js/main.js
@@ -1,21 +1,21 @@
-const originalListOfSuggestions=[
-    'SELECT','FROM','WHERE','JOIN','ON','=','<','>', 'AND', 'IN', 'NOT IN','<=', '>=', '<>','</>']
-words=$()
-let globalselectPart=undefined
-let globalFromPart=undefined
-let globalJoinPart=undefined
-let globalJoinOnPart=undefined
-let globalWherePart=undefined
+const originalListOfSuggestions = [
+    'SELECT', 'FROM', 'WHERE', 'JOIN', 'ON', '=', '<', '>', 'AND', 'IN', 'NOT IN', '<=', '>=', '<>', '</>']
+words = $()
+let globalselectPart = undefined
+let globalFromPart = undefined
+let globalJoinPart = undefined
+let globalJoinOnPart = undefined
+let globalWherePart = undefined
 $('#mainInput').on('input', function () {
-    let words=$(this).val().split(' ')
-    let lastWord=words[words.length-1].toUpperCase().trim()
-    let listSugestions =lastWord !== '' ? originalListOfSuggestions.filter((e) => e.startsWith(lastWord)) :[]
-    let suggestions=$('#commands')
+    let words = $(this).val().split(' ')
+    let lastWord = words[words.length - 1].toUpperCase().trim()
+    let listSugestions = lastWord !== '' ? originalListOfSuggestions.filter((e) => e.startsWith(lastWord)) : []
+    let suggestions = $('#commands')
     suggestions.html('')
-    for(let e of listSugestions) {
+    for (let e of listSugestions) {
         suggestions.append($(`<li onclick="changingInput('${e}')">`).append(`${e}`))
     }
-    if(listSugestions.length === 0) {
+    if (listSugestions.length === 0) {
         $('#commands').hide()
     }
     else {
@@ -31,17 +31,17 @@ function upper() {
     input.selectionEnd = oldPosition;
 }
 function changingInput(value) {
-    $('#mainInput').val( $('#mainInput').val().trim())
-    let words=$('#mainInput').val().split(' ')
-    let lastWord=words[words.length-1].toUpperCase().trim()
-    let newVal=($('#mainInput').val().slice(0,$('#mainInput').val().toUpperCase().lastIndexOf(lastWord))).trim() + ' ' + value
+    $('#mainInput').val($('#mainInput').val().trim())
+    let words = $('#mainInput').val().split(' ')
+    let lastWord = words[words.length - 1].toUpperCase().trim()
+    let newVal = ($('#mainInput').val().slice(0, $('#mainInput').val().toUpperCase().lastIndexOf(lastWord))).trim() + ' ' + value
     $('#mainInput').val(newVal + ' ')
     $('#commands').hide()
     $('#mainInput').focus()
 }
 function validation() {
-    let test=validatingText()
-    if(!test) {
+    let test = validatingText()
+    if (!test) {
         $("#validated-message").show()
         $("#validated-message").html('invalida')
         $("#validated-message").show()
@@ -67,8 +67,8 @@ function validation() {
 
 }
 function validatingText() {
-    let userInput=$('#mainInput').val().trim().toUpperCase()
-    const regex =/^SELECT\s+(.+?)\s+FROM\s+(.+?)(\s+JOIN\s+(.+?)\s+ON\s+(.+?))?\s*(?:WHERE\s+(.+?))?\s?([;])?$/i
+    let userInput = $('#mainInput').val().trim().toUpperCase()
+    const regex = /^SELECT\s+(.+?)\s+FROM\s+(.+?)(\s+JOIN\s+(.+?)\s+ON\s+(.+?))?\s*(?:WHERE\s+(.+?))?\s?([;])?$/i
     const match = userInput.match(regex);
     if (!match) {
         return false
@@ -79,84 +79,90 @@ function validatingText() {
     let joinOnClause = match[5];
     let whereClause = match[6];
     console.log('select:');
-    console.log(selectClause); 
-    console.log('depois da removeracentos:'); 
-    selectClause=tirarAcento(selectClause) 
+    console.log(selectClause);
+    console.log('depois da removeracentos:');
+    selectClause = tirarAcento(selectClause)
     console.log(selectClause);
     console.log('from');
-    console.log(fromClause); 
-    console.log('depois da removeracentos:'); 
-    fromClause=tirarAcento(fromClause) 
-    console.log(fromClause); 
+    console.log(fromClause);
+    console.log('depois da removeracentos:');
+    fromClause = tirarAcento(fromClause)
+    console.log(fromClause);
     console.log('join')
     console.log(joinClause)
-    console.log('depois da removeracentos:'); 
-    joinClause=tirarAcento(joinClause) 
-    console.log(joinClause)
+    if (joinClause) {
+        console.log('depois da removeracentos:');
+        joinClause = tirarAcento(joinClause)
+        console.log(joinClause)
+    }
     console.log('joinon')
     console.log(joinOnClause)
-    console.log('depois da removeracentos:'); 
-    joinOnClause=tirarAcento(joinOnClause) 
-    console.log(joinOnClause)
+    if (joinOnClause) {
+        console.log('depois da removeracentos:');
+        joinOnClause = tirarAcento(joinOnClause)
+        console.log(joinOnClause)
+    }
     console.log('where')
     console.log(whereClause)
-    console.log('depois da removeracentos:'); 
-    whereClause=tirarAcento(whereClause)  
-    console.log(whereClause)
+    if (whereClause) {
+        console.log('depois da removeracentos:');
+        whereClause = tirarAcento(whereClause)
+        console.log(whereClause)
+    }
     console.log('----------------------------------');
-    const selectRegex=/^\s*(\*|\b(?!(?:FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)\b)[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)?(?:,\s*[a-zA-Z0-9_-]+\s*(?:\.[a-zA-Z0-9_-]+)?)*?)\s*$/gi
-    const selectPart=selectClause.match(selectRegex)
-    if(!selectPart) {
+    const selectRegex = /^\s*(\*|\b(?!(?:FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)\b)[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)?(?:,\s*[a-zA-Z0-9_-]+\s*(?:\.[a-zA-Z0-9_-]+)?)*?)\s*$/gi
+    const selectPart = selectClause.match(selectRegex)
+    if (!selectPart) {
         return false
     }
     console.log(selectPart[0]);
-    globalselectPart=selectPart[0]
-    const fromRegex=/^(?!\d)\s?(?!FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)(?!.*[^-_a-zA-Z0-9\s]).*[a-zA-Z][-_a-zA-Z0-9]*$/gi
-    const fromPart=fromClause.match(fromRegex)
-    if(!fromPart) {
+    globalselectPart = selectPart[0]
+    const fromRegex = /^(?!\d)\s?(?!FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)(?!.*[^-_a-zA-Z0-9\s]).*[a-zA-Z][-_a-zA-Z0-9]*$/gi
+    const fromPart = fromClause.match(fromRegex)
+    if (!fromPart) {
         return false
     }
-    globalFromPart=fromPart[0]
+    globalFromPart = fromPart[0]
     console.log(match)
-    if(joinClause) {
-        if(!joinOnClause) {
+    if (joinClause) {
+        if (!joinOnClause) {
             return false
         }
-        if(!joinClause.match(/^(?!\d)\s?(?!FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)(?!.*[^-_a-zA-Z0-9\s]).*[a-zA-Z][-_a-zA-Z0-9]*$/gi) || joinClause.split(' ').length >2) {
+        if (!joinClause.match(/^(?!\d)\s?(?!FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)(?!.*[^-_a-zA-Z0-9\s]).*[a-zA-Z][-_a-zA-Z0-9]*$/gi) || joinClause.split(' ').length > 2) {
             return false
         }
-        let joinPart=[]
-        let joinOnPart=[]
-        let currentJoinOnClause= joinOnClause
-        let currentJoinClause=joinClause
-        let joinObject=joinCheck(currentJoinClause,currentJoinOnClause,joinPart,joinOnPart)
-        if(!joinObject) {
+        let joinPart = []
+        let joinOnPart = []
+        let currentJoinOnClause = joinOnClause
+        let currentJoinClause = joinClause
+        let joinObject = joinCheck(currentJoinClause, currentJoinOnClause, joinPart, joinOnPart)
+        if (!joinObject) {
             alert('query invalida')
             return false
         }
         else {
-            joinPart=joinObject.joinPart
-            joinOnPart=joinObject.joinOnPart
-            joinPart=joinPart.filter((e,i) => { return joinPart.indexOf(e) === i})
-            joinOnPart=joinOnPart.filter((element,index) => { return  joinOnPart.indexOf(element) === index})
-            globalJoinPart=joinPart
-            globalJoinOnPart=joinOnPart
+            joinPart = joinObject.joinPart
+            joinOnPart = joinObject.joinOnPart
+            joinPart = joinPart.filter((e, i) => { return joinPart.indexOf(e) === i })
+            joinOnPart = joinOnPart.filter((element, index) => { return joinOnPart.indexOf(element) === index })
+            globalJoinPart = joinPart
+            globalJoinOnPart = joinOnPart
             console.log('join array');
             console.log(joinPart);
             console.log('joinOn array');
             console.log(joinOnPart);
         }
     }
-    if(whereClause) {
-        let wherepart=[]
-        let currentWhereClause=whereClause
-        let whereTest=whereCheck(currentWhereClause,wherepart)
-        if(!whereTest) {
+    if (whereClause) {
+        let wherepart = []
+        let currentWhereClause = whereClause
+        let whereTest = whereCheck(currentWhereClause, wherepart)
+        if (!whereTest) {
             return false
         }
         console.log('where part:');
         console.log(wherepart);
-        globalWherePart=wherepart
+        globalWherePart = wherepart
     }
     return true
 
@@ -164,71 +170,71 @@ function validatingText() {
 function removeDuplicates(arr) {
     return [...new Set(arr)];
 }
-function tirarAcento(string1) { 
+function tirarAcento(string1) {
     return string1.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
 }
-function joinCheck(currentJoinClause,currentJoinOnClause,joinPart,joinOnPart) {
-        joinPart.push(currentJoinClause.trim().toUpperCase())
-        let aux=currentJoinOnClause.trim().toUpperCase().indexOf(' JOIN ')
-        let currentJoinOn;
-        if(aux !==  -1) {
-            let joinOn=currentJoinOnClause.slice(0,aux)
-            let currentjoinOnPart=joinOn.match(/^(?!FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)([a-zA-Z]+[\w.]*)\s*(=|>|<|<=|>=|<>)\s*([a-zA-Z]+[\w.]*)$/gi)
-            if(!currentjoinOnPart || currentjoinOnPart.length >2) {
-                return undefined
-            }
-            joinOnPart.push(currentjoinOnPart[0])
-            let nextClause=currentJoinOnClause.slice(aux,currentJoinOnClause.length).trim().toUpperCase()
-            if(!nextClause) {
-                return undefined
-            }
-            let nextAux=nextClause.indexOf(' ON ')
-            if(nextAux === -1) {
-                return undefined
-            }
-            let nextJoinClause=nextClause.slice(0,nextAux).replace('JOIN','').trim()
-            let nextJoinOnClause=nextClause.slice(nextAux,nextClause.length).replace('ON','').trim()
-            let nextObject=joinCheck(nextJoinClause,nextJoinOnClause,joinPart,joinOnPart)
-            if(!nextObject) {
-                return undefined
-            }
-            joinPart=joinPart.concat(nextObject.joinPart)
-            joinOnPart=joinOnPart.concat(nextObject.joinOnPart)
-            return  {
-                joinPart:joinPart,
-                joinOnPart:joinOnPart
-            }
+function joinCheck(currentJoinClause, currentJoinOnClause, joinPart, joinOnPart) {
+    joinPart.push(currentJoinClause.trim().toUpperCase())
+    let aux = currentJoinOnClause.trim().toUpperCase().indexOf(' JOIN ')
+    let currentJoinOn;
+    if (aux !== -1) {
+        let joinOn = currentJoinOnClause.slice(0, aux)
+        let currentjoinOnPart = joinOn.match(/^(?!FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)([a-zA-Z]+[\w.]*)\s*(=|>|<|<=|>=|<>)\s*([a-zA-Z]+[\w.]*)$/gi)
+        if (!currentjoinOnPart || currentjoinOnPart.length > 2) {
+            return undefined
         }
-        else {
-            currentJoinOn=currentJoinOnClause
-            let currentjoinOnPart=currentJoinOn.match(/^(?!FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)([a-zA-Z]+[\w.]*)\s*(=|>|<|<=|>=|<>)\s*([a-zA-Z]+[\w.]*)$/gi)
-            if(!currentjoinOnPart || currentjoinOnPart.length >2) {
-                return undefined
-            }
-            joinOnPart.push(currentjoinOnPart[0])
-            return  {
-                joinPart:joinPart,
-                joinOnPart:joinOnPart
-            }
+        joinOnPart.push(currentjoinOnPart[0])
+        let nextClause = currentJoinOnClause.slice(aux, currentJoinOnClause.length).trim().toUpperCase()
+        if (!nextClause) {
+            return undefined
         }
+        let nextAux = nextClause.indexOf(' ON ')
+        if (nextAux === -1) {
+            return undefined
+        }
+        let nextJoinClause = nextClause.slice(0, nextAux).replace('JOIN', '').trim()
+        let nextJoinOnClause = nextClause.slice(nextAux, nextClause.length).replace('ON', '').trim()
+        let nextObject = joinCheck(nextJoinClause, nextJoinOnClause, joinPart, joinOnPart)
+        if (!nextObject) {
+            return undefined
+        }
+        joinPart = joinPart.concat(nextObject.joinPart)
+        joinOnPart = joinOnPart.concat(nextObject.joinOnPart)
+        return {
+            joinPart: joinPart,
+            joinOnPart: joinOnPart
+        }
+    }
+    else {
+        currentJoinOn = currentJoinOnClause
+        let currentjoinOnPart = currentJoinOn.match(/^(?!FROM|SELECT|JOIN|ON|AND|WHERE|IN|NOT)([a-zA-Z]+[\w.]*)\s*(=|>|<|<=|>=|<>)\s*([a-zA-Z]+[\w.]*)$/gi)
+        if (!currentjoinOnPart || currentjoinOnPart.length > 2) {
+            return undefined
+        }
+        joinOnPart.push(currentjoinOnPart[0])
+        return {
+            joinPart: joinPart,
+            joinOnPart: joinOnPart
+        }
+    }
 }
-function whereCheck(currentWhereClause,wherePart) {
-    let aux=currentWhereClause.trim().toUpperCase().indexOf('AND')
-    if(aux !== -1) {
-        let originalWhere=currentWhereClause
-        currentWhereClause=originalWhere.slice(0,aux).trim().toUpperCase()
-        currentWhereClause=currentWhereClause.match(/^(?!FROM|SELECT|JOIN|ON|AND|WHERE)([a-zA-Z]+[\w]*(?:[.]?[a-zA-Z]+[\w]*)?)\s*(?:(=|<=|>=|<>|>|<)\s*([a-zA-Z]+[\w]*(?:[.]?[a-zA-Z]+[\w]*)?|\d+|(?:[']{1}(?:\w*\s*)+[']{1}){1})|\s*(IN|NOT\s*IN)\s*([(]{1}\s*(?:[']{1}\w*[']{1}|\d+){1}(?:(?:\s)*[,]{1}(?:\s)*(?:[']{1}\w*[']{1}|\d+){1})*\s*[)]{1}))$/gi)
-        if(!currentWhereClause) {
+function whereCheck(currentWhereClause, wherePart) {
+    let aux = currentWhereClause.trim().toUpperCase().indexOf('AND')
+    if (aux !== -1) {
+        let originalWhere = currentWhereClause
+        currentWhereClause = originalWhere.slice(0, aux).trim().toUpperCase()
+        currentWhereClause = currentWhereClause.match(/^(?!FROM|SELECT|JOIN|ON|AND|WHERE)([a-zA-Z]+[\w]*(?:[.]?[a-zA-Z]+[\w]*)?)\s*(?:(=|<=|>=|<>|>|<)\s*([a-zA-Z]+[\w]*(?:[.]?[a-zA-Z]+[\w]*)?|\d+|(?:[']{1}(?:\w*\s*)+[']{1}){1})|\s*(IN|NOT\s*IN)\s*([(]{1}\s*(?:[']{1}\w*[']{1}|\d+){1}(?:(?:\s)*[,]{1}(?:\s)*(?:[']{1}\w*[']{1}|\d+){1})*\s*[)]{1}))$/gi)
+        if (!currentWhereClause) {
             return undefined
         }
         console.log(currentWhereClause)
         wherePart.push(currentWhereClause[0]);
-        let nextWhereClause=originalWhere.slice(aux,originalWhere.length).replace('AND','').trim().toUpperCase()
-        wherePart=whereCheck(nextWhereClause,wherePart);
+        let nextWhereClause = originalWhere.slice(aux, originalWhere.length).replace('AND', '').trim().toUpperCase()
+        wherePart = whereCheck(nextWhereClause, wherePart);
         return wherePart;
-    }else {
-        currentWhereClause=currentWhereClause.match(/^(?!FROM|SELECT|JOIN|ON|AND|WHERE)([a-zA-Z]+[\w]*(?:[.]?[a-zA-Z]+[\w]*)?)\s*(?:(=|<=|>=|<>|>|<)\s*([a-zA-Z]+[\w]*(?:[.]?[a-zA-Z]+[\w]*)?|\d+|(?:[']{1}(?:\w*\s*)+[']{1}){1})|\s*(IN|NOT\s*IN)\s*([(]{1}\s*(?:[']{1}\w*[']{1}|\d+){1}(?:(?:\s)*[,]{1}(?:\s)*(?:[']{1}\w*[']{1}|\d+){1})*\s*[)]{1}))$/gi)
-        if(!currentWhereClause) {
+    } else {
+        currentWhereClause = currentWhereClause.match(/^(?!FROM|SELECT|JOIN|ON|AND|WHERE)([a-zA-Z]+[\w]*(?:[.]?[a-zA-Z]+[\w]*)?)\s*(?:(=|<=|>=|<>|>|<)\s*([a-zA-Z]+[\w]*(?:[.]?[a-zA-Z]+[\w]*)?|\d+|(?:[']{1}(?:\w*\s*)+[']{1}){1})|\s*(IN|NOT\s*IN)\s*([(]{1}\s*(?:[']{1}\w*[']{1}|\d+){1}(?:(?:\s)*[,]{1}(?:\s)*(?:[']{1}\w*[']{1}|\d+){1})*\s*[)]{1}))$/gi)
+        if (!currentWhereClause) {
             return undefined
         }
         console.log(currentWhereClause)
@@ -248,7 +254,7 @@ function juncao_e_reducao_tuplas() {
         if (i == 0) {
             let replace_table_from = []
             let replace_table_join = []
-            for (let j=0; j< globalWherePart.length; j++) {
+            for (let j = 0; j < globalWherePart.length; j++) {
                 let current_table = globalWherePart[j][0].split(' ')[0].split('.')[0]
                 if (current_table === globalFromPart) {
                     replace_table_from.push(globalWherePart[j])
@@ -273,8 +279,8 @@ function juncao_e_reducao_tuplas() {
             jun += `(${part_1} |x| ${globalJoinOnPart[i]} ${part_2})`;
         } else {
             let replace_table_join = []
-            for (let j=0; j< globalWherePart.length; j++) {
-                if (globalWherePart[j] === undefined ) { continue }
+            for (let j = 0; j < globalWherePart.length; j++) {
+                if (globalWherePart[j] === undefined) { continue }
                 console.log(globalWherePart)
                 let current_table = globalWherePart[j][0].split(' ')[0].split('.')[0]
                 if (current_table === globalJoinPart[i]) {
